9197791024f477bcb2ec13ac9adf2c431915268e
s10243563@connect.np.edu.sg <s10243563@connect.np.edu.sg>
2022-07-28 14:53:30 +0800

    ad

--- api.py
+++ api.py
@@ -6,5 +6,7 @@ api_key="7NJXMGNT452HK20E"
 url = 'https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey={api_key}'
 r = requests.get(url)
 data = r.json()
-print(data)
+#print(data)
 data_str = str(data)
+
+excravg = re.findall(pattern = "'4. close': '[0-9].[0-9]+'", string = data_str)
