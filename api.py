e9b53ba48a6c22cedd1ec0e85cd46f1055ad69fb
SJCGavin <S10239957@connect.np.edu.sg>
2022-07-28 14:41:24 +0800

    API KEY CHANGE

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import re
 
 api_key="7NJXMGNT452HK20E"
-url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}'
+url = 'https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey={api_key}'
 r = requests.get(url)
 data = r.json()
 print(data)
