2c157b10fcd99539dd5a7f65d96d54b394ac2b73
SJCGavin <S10239957@connect.np.edu.sg>
2022-07-28 14:29:11 +0800

    Import

--- read_file.py
+++ read_file.py
@@ -1,2 +1,3 @@
+import re
 exchr = re.findall(pattern = "[0-9].[0-9]+", string = data_str)
 print(exchr[0])
--- write_file.py
+++ write_file.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 fp = Path.cwd()
 print(fp.exists())
 fp_txt = Path.cwd()/(r"deficit_report.txt")
