0126b2e8c250d2379abed9a673fd7ecf60a1189d
s10243563@connect.np.edu.sg <s10243563@connect.np.edu.sg>
2022-07-28 12:36:26 +0800

    wdw

--- Main.py
+++ Main.py
@@ -1,15 +1,17 @@
 import requests
 from pathlib import Path
 import csv
+import re
 
 api_key="7NJXMGNT452HK20E"
 url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}'
 r = requests.get(url)
 data = r.json()
-
 print(data)
+
 fp = Path.cwd()
 print(fp.exists())
 fp_txt = Path.cwd()/(r"deficit_report.txt")
 fp_txt.touch()
 print(fp_txt.exists())
+
