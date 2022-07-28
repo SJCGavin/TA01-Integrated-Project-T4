2cc17e804b286dd55e0711f8bcef5fa5efde38cc
jingjingggggggg <s10221116@connect.np.edu.sg>
2022-07-28 12:00:14 +0800

    h

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
-from xml.dom import UserDataHandler
-import requests
+
 api_key="7NJXMGNT452HK20E"
 url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}'
 r = requests.get(url)
