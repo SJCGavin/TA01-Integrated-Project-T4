de83cc5e6a5a497b12e72a8d9c3fb7f9e6fe971e
SJCGavin <S10239957@connect.np.edu.sg>
2022-07-28 11:45:15 +0800

    Main.py currency exhange import

--- Main.py
+++ Main.py
@@ -1,3 +1,7 @@
-import moda
-print("hello world")
-print("poopooheadhead")
+import requests
+api_key="7NJXMGNT452HK20E"
+url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}'
+r = requests.get(url)
+data = r.json()
+
+print(data)
