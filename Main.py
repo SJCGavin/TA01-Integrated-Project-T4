8d8899f01899e7cb2134eed7b43b054691af0c74
s10243563@connect.np.edu.sg <s10243563@connect.np.edu.sg>
2022-07-28 12:47:13 +0800

    h

--- Main.py
+++ Main.py
@@ -8,10 +8,14 @@ url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_cu
 r = requests.get(url)
 data = r.json()
 print(data)
+data_str = str(data)
 
 fp = Path.cwd()
 print(fp.exists())
 fp_txt = Path.cwd()/(r"deficit_report.txt")
 fp_txt.touch()
 print(fp_txt.exists())
+
+exchr = re.findall(pattern = "[0-9].[0-9]+", string = data_str)
+print(exchr[0])
 
