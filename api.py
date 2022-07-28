fbd0510ebd572d097d813844f78f6490036b5d62
s10243563@connect.np.edu.sg <s10243563@connect.np.edu.sg>
2022-07-28 15:10:10 +0800

    yay!

--- api.py
+++ api.py
@@ -10,3 +10,7 @@ data = r.json()
 data_str = str(data)
 
 excravg = re.findall(pattern = "'4. close': '[0-9].[0-9]+'", string = data_str)
+excravg_new = str(excravg)
+excravg_strip = excravg_new.replace("'4. close': '", "")
+excravg_strip_1 = excravg_strip.replace("'", "")
+print(excravg_strip_1)
