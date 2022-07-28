2226ff76e0bc137d2515443bce1470b706b8ce7b
SJCGavin <S10239957@connect.np.edu.sg>
2022-07-28 15:09:39 +0800

    reader for CAH USD

--- read_file.py
+++ read_file.py
@@ -2,12 +2,12 @@
 # exchr = re.findall(pattern = "[0-9].[0-9]+", string = data_str)
 # print(exchr[0])
 
-def api():
-    from pathlib import Path
-    import csv
-    import re
-    fp = Path.cwd()
-    for file in fp.glob("*.csv"):
-        with file.open (mode="r", encoding = "UTF-8") as file:
-            text = file.read()
-        print (text)
+
+from pathlib import Path
+import csv
+import re
+fp = Path.cwd()/"csv_reports"/"Cash-on-hand-usd.csv"
+with fp.open (mode="r", encoding = "UTF-8", newline="") as file1:
+    reader= csv.reader(file1)
+    for line in reader:
+        print(line)
