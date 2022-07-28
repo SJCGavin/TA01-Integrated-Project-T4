df4639fc1a1b49126813ebdc7224c61b57e3a577
s10243563@connect.np.edu.sg <s10243563@connect.np.edu.sg>
2022-07-28 12:00:57 +0800

    Message: NIL

--- Main.py
+++ Main.py
@@ -1,3 +1,6 @@
+import requests
+from pathlib import Path
+import csv
 
 api_key="7NJXMGNT452HK20E"
 url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}'
