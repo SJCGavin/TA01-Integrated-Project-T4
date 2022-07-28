43bb794a5118954cefc66905991784f731ddd468
celeste1201 <s10228967@connect.np.edu.sg>
2022-07-28 14:38:57 +0800

    hello

--- write_file.py
+++ write_file.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+from winreg import HKEY_LOCAL_MACHINE
 fp = Path.cwd()
 print(fp.exists())
 fp_txt = Path.cwd()/(r"deficit_report.txt")
 fp_txt.touch()
 print(fp_txt.exists())
+print("hello")
