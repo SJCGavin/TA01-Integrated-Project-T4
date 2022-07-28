0f21c9bb7f1149a7812839564b8dd8eb3c5d7ea3
s10243563@connect.np.edu.sg <s10243563@connect.np.edu.sg>
2022-07-28 14:11:39 +0800

    d

--- api.py
+++ api.py
@@ -1,1 +1,8 @@
+import requests
 
+api_key="7NJXMGNT452HK20E"
+url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}'
+r = requests.get(url)
+data = r.json()
+print(data)
+data_str = str(data)
