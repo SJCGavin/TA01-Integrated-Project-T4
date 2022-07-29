6bed2ec00f3875799c32d048dd171c8e9a649fa8
CobyJacob <s10242150@connect.np.edu.sg>
2022-07-29 12:51:43 +0800

    message

--- read_file.py
+++ read_file.py
@@ -5,7 +5,6 @@
 
 from pathlib import Path
 import csv
-import re
 fp = Path.cwd()/"TA01-Integrated-Project-T4"/"csv_reports"/"Cash-on-hand-usd.csv"
 empty_list = []
 with fp.open (mode="r", encoding = "UTF-8", newline="") as file1:
@@ -14,8 +13,4 @@ with fp.open (mode="r", encoding = "UTF-8", newline="") as file1:
     for line in reader:
         empty_list.append(line)
 for list in empty_list:
-    print(list[1])
-    # for line in reader:
-    #     print(line)
-
-    
+    print(list)
