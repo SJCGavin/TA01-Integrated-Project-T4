f608cc80cdcc234735a5f604f5d2d086ccd1dc02
s10243563@connect.np.edu.sg <s10243563@connect.np.edu.sg>
2022-08-01 13:55:18 +0800

    new

--- read_file.py
+++ read_file.py
@@ -5,46 +5,49 @@
 
 from pathlib import Path
 import csv
-fp = Path.cwd()/"csv_reports"/"Profit-and-loss-usd.csv"
-list = []
-with fp.open (mode="r", encoding = "UTF-8", newline="") as file1:
-    reader = csv.reader(file1)
-    next(reader)
-    for line in reader:
-        empty_list.append(line)
-        profit_loss = (line[0],line[3])
-        list.append(profit_loss)
-def profit_loss_data():
-    days = 0
-    cash=0
-    data = []
-    for line in list:
-        if days == 0:
-            days = float(line[0]) + 1
-            cash = float(line[1])
-        elif days < float(line[0]) + 1:
-            if cash > float(line[1]):
-                    return line
-        elif days > float(line[0]) + 1:
-            if cash < float(line[1]):
-                data.append(line)
-        elif days < float(line[0]) + 1:
-            if cash > float(line[1]):
-                return "Net profit on each period is higher than the previous period"
-print(profit_loss_data())
+# empty_list = []
+# fp = Path.cwd()/"csv_reports"/"Profit-and-loss-usd.csv"
+# list = []
+# with fp.open (mode="r", encoding = "UTF-8", newline="") as file1:
+#     reader = csv.reader(file1)
+#     next(reader)
+#     for line in reader:
+#         empty_list.append(line)
+#         profit_loss = (line[0],line[3])
+#         list.append(profit_loss)
 
-listy = []
-fp1 = Path.cwd()/"csv_reports"/"Overheads-day-45.csv"
-def overheady():
-    with fp1.open(mode="r", encoding="UTF-8", newline="") as file:
-        reader = csv.reader(file)
-        next(reader)
-        for line in reader:
-            lines = float(line[1])
-            listy.append(lines)
-        listy.sort()
-        print(listy)
-        print(listy[-1])
+# def profit_loss_data():
+#     days = 0
+#     cash=0
+#     data = []
+#     for line in list:
+#         if days == 0:
+#             days = float(line[0]) + 1
+#             cash = float(line[1])
+#         elif days < float(line[0]) + 1:
+#             if cash > float(line[1]):
+#                     return line
+#         elif days > float(line[0]) + 1:
+#             if cash < float(line[1]):
+#                 data.append(line)
+#         elif days < float(line[0]) + 1:
+#             if cash > float(line[1]):
+#                 return "Net profit on each period is higher than the previous period"
+# print(profit_loss_data())
+
+
+# listy = []
+# fp1 = Path.cwd()/"csv_reports"/"Overheads-day-45.csv"
+# def overheady():
+#     with fp1.open(mode="r", encoding="UTF-8", newline="") as file:
+#         reader = csv.reader(file)
+#         next(reader)
+#         for line in reader:
+#             lines = float(line[1])
+#             listy.append(lines)
+#         listy.sort()
+#         print(listy[-1])
+# print(overheady())
         
 fp = Path.cwd()/"csv_reports"/"Cash-on-hand-usd.csv"
 def cashdata():
@@ -70,4 +73,3 @@ def cashdata():
             elif days > float(line[0]) + 1:
                 if cash < float(line[0]):
                     data.append(line)
-print(cashdata())
--- write_file.py
+++ write_file.py
@@ -1,8 +1,13 @@
 from pathlib import Path
 from winreg import HKEY_LOCAL_MACHINE
+
+from read_file import cashdata
+
 fp = Path.cwd()
-print(fp.exists())
 fp_txt = Path.cwd()/(r"deficit_report.txt")
 fp_txt.touch()
-print(fp_txt.exists())
-print("hello")
+# print(fp_txt.exists())
+
+
+
+print(cashdata())
