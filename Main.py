3b86b8f3163e044d6a4ed252dee9261134fe18d6
jingjingggggggg <s10221116@connect.np.edu.sg>
2022-08-01 13:28:52 +0800

    move

--- Main.py
+++ Main.py
@@ -22,31 +22,31 @@ import re
 #     print(data)
 
 
-# fp = Path.cwd()/"csv_reports"/"Cash-on-hand-usd.csv"
-# def cashdata():
-#     with fp.open(mode="r", encoding="UTF-8", newline="") as file:
-#         reader = csv.reader(file)
-#         next(reader)
-#         '''
-#         Opens file to read data
-#         '''
-#         days = 0
-#         cash = 0
-#         '''
+fp = Path.cwd()/"csv_reports"/"Cash-on-hand-usd.csv"
+def cashdata():
+    with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+        reader = csv.reader(file)
+        next(reader)
+        '''
+        Opens file to read data
+        '''
+        days = 0
+        cash = 0
+        '''
         
-#         '''
-#         data = []
-#         for line in reader:
-#             if days == 0:
-#                 days = float(line[0]) + 1
-#                 cash = float(line[1])
-#             elif days < float(line[0]) + 1:
-#                 if cash > float(line[1]):
-#                     return line
-#             elif days > float(line[0]) + 1:
-#                 if cash < float(line[0]):
-#                     data.append(line)
-# print(cashdata())
+        '''
+        data = []
+        for line in reader:
+            if days == 0:
+                days = float(line[0]) + 1
+                cash = float(line[1])
+            elif days < float(line[0]) + 1:
+                if cash > float(line[1]):
+                    return line
+            elif days > float(line[0]) + 1:
+                if cash < float(line[0]):
+                    data.append(line)
+print(cashdata())
 listy = []
 fp1 = Path.cwd()/"csv_reports"/"Overheads-day-45.csv"
 def overheady():
--- read_file.py
+++ read_file.py
@@ -6,41 +6,6 @@
 from pathlib import Path
 import csv
 fp = Path.cwd()/"csv_reports"/"Profit-and-loss-usd.csv"
-empty_list = []
-# def peepee():
-#     with fp.open (mode="r", encoding = "UTF-8", newline="") as file1:
-#         reader = csv.reader(file1)
-#         next(reader)
-#         '''
-#         Opens file to read data
-#         '''
-#         days = 0
-#         cash = 0
-#         '''
-        
-#         '''
-#         data = []
-#         for line in reader:
-#             if days == 0:
-#                 days = float(line[0]) + 1
-#                 cash = float(line[3])
-#             elif days < float(line[0]) + 1:
-#                 if cash > float(line[3]):
-#                         return line
-#             elif days > float(line[0]) + 1:
-#                 if cash < float(line[3]):
-#                     data.append(line)
-#             elif days < float(line[0]) + 1:
-#                 if cash > float(line[3]):
-#                     return "None"
-#             for line in reader:
-#                 empty_list.append(line)
-#                 poo = (line[0],line[3])
-#                 return poo
-# print(peepee())
-        
-# for list in empty_list:
-#     print(list)
 list = []
 with fp.open (mode="r", encoding = "UTF-8", newline="") as file1:
     reader = csv.reader(file1)
@@ -65,5 +30,44 @@ def profit_loss_data():
                 data.append(line)
         elif days < float(line[0]) + 1:
             if cash > float(line[1]):
-                return "None"
+                return "Net profit on each period is higher than the previous period"
 print(profit_loss_data())
+
+listy = []
+fp1 = Path.cwd()/"csv_reports"/"Overheads-day-45.csv"
+def overheady():
+    with fp1.open(mode="r", encoding="UTF-8", newline="") as file:
+        reader = csv.reader(file)
+        next(reader)
+        for line in reader:
+            lines = float(line[1])
+            listy.append(lines)
+        listy.sort()
+        print(listy)
+        print(listy[-1])
+        
+fp = Path.cwd()/"csv_reports"/"Cash-on-hand-usd.csv"
+def cashdata():
+    with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+        reader = csv.reader(file)
+        next(reader)
+        '''
+        Opens file to read data
+        '''
+        days = 0
+        cash = 0
+        '''
+        
+        '''
+        data = []
+        for line in reader:
+            if days == 0:
+                days = float(line[0]) + 1
+                cash = float(line[1])
+            elif days < float(line[0]) + 1:
+                if cash > float(line[1]):
+                    return line
+            elif days > float(line[0]) + 1:
+                if cash < float(line[0]):
+                    data.append(line)
+print(cashdata())
