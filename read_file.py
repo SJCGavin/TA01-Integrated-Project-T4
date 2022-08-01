d42d676a4f7d50dd2dad8a5bfbd28c00961b9b01
s10243563@connect.np.edu.sg <s10243563@connect.np.edu.sg>
2022-08-01 11:37:01 +0800

    new

--- read_file.py
+++ read_file.py
@@ -7,13 +7,63 @@ from pathlib import Path
 import csv
 fp = Path.cwd()/"csv_reports"/"Profit-and-loss-usd.csv"
 empty_list = []
-with fp.open (mode="r", encoding = "UTF-8", newline="") as file1:
-    reader= csv.reader(file1)
-    next(reader)
-    for line in reader:
-        empty_list.append(line)
-        print(line[0],line[3])
-
+# def peepee():
+#     with fp.open (mode="r", encoding = "UTF-8", newline="") as file1:
+#         reader = csv.reader(file1)
+#         next(reader)
+#         '''
+#         Opens file to read data
+#         '''
+#         days = 0
+#         cash = 0
+#         '''
+        
+#         '''
+#         data = []
+#         for line in reader:
+#             if days == 0:
+#                 days = float(line[0]) + 1
+#                 cash = float(line[3])
+#             elif days < float(line[0]) + 1:
+#                 if cash > float(line[3]):
+#                         return line
+#             elif days > float(line[0]) + 1:
+#                 if cash < float(line[3]):
+#                     data.append(line)
+#             elif days < float(line[0]) + 1:
+#                 if cash > float(line[3]):
+#                     return "None"
+#             for line in reader:
+#                 empty_list.append(line)
+#                 poo = (line[0],line[3])
+#                 return poo
+# print(peepee())
         
 # for list in empty_list:
 #     print(list)
+list = []
+with fp.open (mode="r", encoding = "UTF-8", newline="") as file1:
+    reader = csv.reader(file1)
+    next(reader)
+    for line in reader:
+        empty_list.append(line)
+        poo = (line[0],line[3])
+        list.append(poo)
+def peepee():
+    days = 0
+    cash=0
+    data = []
+    for line in list:
+        if days == 0:
+            days = float(line[0]) + 1
+            cash = float(line[1])
+        elif days < float(line[0]) + 1:
+            if cash > float(line[1]):
+                    return line
+        elif days > float(line[0]) + 1:
+            if cash < float(line[1]):
+                data.append(line)
+        elif days < float(line[0]) + 1:
+            if cash > float(line[1]):
+                return "None"
+print(peepee())
