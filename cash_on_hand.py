0fa57f5005ff20a0d09f6f3683a97a41b6acef82
CobyJacob <s10242150@connect.np.edu.sg>
2022-08-01 15:12:41 +0800

    da

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 import csv
 
-fp = Path.cwd()/"TA01-Integrated-Project-T4"/"csv_reports"/"Cash-on-hand-usd.csv"
+fp_coh = Path.cwd()/"TA01-Integrated-Project-T4"/"csv_reports"/"Cash-on-hand-usd.csv"
 def cashdata():
-    with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    with fp_coh.open(mode="r", encoding="UTF-8", newline="") as file:
         reader = csv.reader(file)
         next(reader)
         '''
@@ -14,7 +14,7 @@ def cashdata():
         '''
         
         '''
-        data = []
+        empty_list_coh = []
         for line in reader:
             if days == 0:
                 days = float(line[0]) + 1
@@ -24,5 +24,5 @@ def cashdata():
                     return line
             elif days > float(line[0]) + 1:
                 if cash < float(line[0]):
-                    data.append(line)
+                    empty_list_coh.append(line)
 print(cashdata())
