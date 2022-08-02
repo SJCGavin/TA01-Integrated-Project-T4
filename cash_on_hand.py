97a400e0c3a2d12661e8096d28f2ebe30c151615
CobyJacob <s10242150@connect.np.edu.sg>
2022-08-02 16:26:12 +0800

    message

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,28 +1,39 @@
 from pathlib import Path
 import csv
 
-fp_coh = Path.cwd()/"csv_reports"/"Cash-on-hand-usd.csv"
-def cashdata():
-    with fp_coh.open(mode="r", encoding="UTF-8", newline="") as file:
-        reader = csv.reader(file)
-        next(reader)
-        '''
-        Opens file to read data
-        '''
-        days = 0
-        cash = 0
-        '''
+lis = []
+
+fp_coh = Path.cwd()/"TA01-Integrated-Project-T4"/"csv_reports"/"Cash-on-hand-usd.csv"
+
+with fp_coh.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(reader)
+    for line in reader:
+        lis.append(line)
+for i in range(1, len(lis)-1):
+    if lis[i][1] < lis[i-1][1]:
+        print(lis[i])
+    elif
+        print("CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+
+
+#         '''
+#         Opens file to read data
+#         '''
+#         days = 0
+#         cash = 0
+#         '''
         
-        '''
-        empty_list_coh = []
-        for line in reader:
-            if days == 0:
-                days = float(line[0]) + 1
-                cash = float(line[1])
-            elif days < float(line[0]) + 1:
-                if cash > float(line[1]):
-                    return line
-            elif days > float(line[0]) + 1:
-                if cash < float(line[0]):
-                    empty_list_coh.append(line)
-print(cashdata())
+#         '''
+#         empty_list_coh = []
+#         for line in reader:
+#             if days == 0:
+#                 days = float(line[0]) + 1
+#                 cash = float(line[1])
+#             elif days < float(line[0]) + 1:
+#                 if cash > float(line[1]):
+#                     return line
+#             elif days > float(line[0]) + 1:
+#                 if cash < float(line[0]):
+#                     empty_list_coh.append(line)
+# print(cashdata())
--- profit_loss.py
+++ profit_loss.py
@@ -1,30 +1,35 @@
 from pathlib import Path
 import csv
 empty_list_pnl = []
-fp_pnl = Path.cwd()/"csv_reports"/"Profit-and-loss-usd.csv"
+empty_list_pnl2 = []
+fp_pnl = Path.cwd()/"TA01-Integrated-Project-T4"/"csv_reports"/"Profit-and-loss-usd.csv"
 with fp_pnl.open (mode="r", encoding = "UTF-8", newline="") as file1:
     reader = csv.reader(file1)
     next(reader)
     for line in reader:
         empty_list_pnl.append(line)
         profit_loss = (line[0],line[3])
-        empty_list_pnl.append(profit_loss)
+        empty_list_pnl2.append(profit_loss)
+for i in range(1, len(empty_list_pnl2)-1):
+    if float(empty_list_pnl2[i][1]) < float(empty_list_pnl2[i-1][1]):
+        print(float(empty_list_pnl2[i]))
+    elif float(empty_list_pnl2[i][1]) == "0":
+        print("CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+# def profit_loss_data():
+#     days = 0
+#     cash=0
+#     for line in empty_list_pnl:
+#         if days == 0:
+#             days = float(line[0]) + 1
+#             cash = float(line[1])
+#         elif days < float(line[0]) + 1:
+#             if cash > float(line[1]):
+#                 return "Net profit on each period is higher than the previous period"
+#         elif days > float(line[0]) + 1:
+#             if cash < float(line[1]):
+#                 return line
+#         elif days < float(line[0]) + 1:
+#             if cash > float(line[1]):
+#                 return line
 
-def profit_loss_data():
-    days = 0
-    cash=0
-    for line in empty_list_pnl:
-        if days == 0:
-            days = float(line[0]) + 1
-            cash = float(line[1])
-        elif days < float(line[0]) + 1:
-            if cash > float(line[1]):
-                return "Net profit on each period is higher than the previous period"
-        elif days > float(line[0]) + 1:
-            if cash < float(line[1]):
-                return line
-        elif days < float(line[0]) + 1:
-            if cash > float(line[1]):
-                return line
-
-print(profit_loss_data())
+# print(profit_loss_data())
