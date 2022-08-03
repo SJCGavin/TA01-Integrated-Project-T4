829befc6e502973757d4bb3a2b49c81a2513a6c9
s10243563@connect.np.edu.sg <s10243563@connect.np.edu.sg>
2022-08-03 16:44:46 +0800

    asdfghjkl

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -17,9 +17,6 @@ def cashdata(forex):
     for i in range(1, len(empty_list_coh)):
         if empty_list_coh[i][1] < empty_list_coh[i-1][1]:
             empty_list_coh2 = (empty_list_coh[i])
-            lis.append(empty_list_coh2)
-            for stuff in lis:
-                int(stuff[1])
         elif empty_list_coh[i][1] > empty_list_coh[i-1][1]:
             count += 1
             if count == len(empty_list_coh):
@@ -27,7 +24,5 @@ def cashdata(forex):
     if empty_list_coh2 == "CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY":
         return "CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
     else:
-        converted_coh = float(lis[1]) * float(forex)
-        return round(float(lis[0]), 1), round(converted_coh, 1)
-
-print(cashdata(1))
+        converted_coh = float(empty_list_coh2[1]) * float(forex)
+        return round(float(empty_list_coh2[0]), 1), round(converted_coh, 1)
