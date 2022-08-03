5b86d6832da406c6015f7aa502fd0a02982d567c
s10243563@connect.np.edu.sg <s10243563@connect.np.edu.sg>
2022-08-03 15:37:02 +0800

    some stuff

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -2,29 +2,32 @@ from pathlib import Path
 import csv
 
 empty_list_coh = []
+lis = []
 
 fp_coh = Path.cwd()/"csv_reports"/"Cash-on-hand-usd.csv"
 
-def cashdata():
+def cashdata(forex):
     with fp_coh.open(mode="r", encoding="UTF-8", newline="") as file:
         reader = csv.reader(file)
         next(reader)
         for line in reader:
             empty_list_coh.append(line)
-        return empty_list_coh   
-print(cashdata())
 
-#     count = 0
-#     for i in range(1, len(empty_list_coh)):
-#         if empty_list_coh[i][1] < empty_list_coh[i-1][1]:
-#             empty_list_coh2 = empty_list_coh[i]
-#         elif empty_list_coh[i][1] > empty_list_coh[i-1][1]:
-#             count += 1
-#             if count == len(empty_list_coh) - 1:
-#                 empty_list_coh2 = "CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
-#     if empty_list_coh2 == "CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY":
-#         return "CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
-#     else:
-#         converted_coh = float(empty_list_coh2[1]) * float(forex)
-#         return round(float(empty_list_coh2[0]), 1), round(converted_coh, 1)
-# print(cashdata(1))
+    count = 0
+    for i in range(1, len(empty_list_coh)):
+        if empty_list_coh[i][1] < empty_list_coh[i-1][1]:
+            empty_list_coh2 = (empty_list_coh[i])
+            lis.append(empty_list_coh2)
+            for stuff in lis:
+                int(stuff[1])
+        elif empty_list_coh[i][1] > empty_list_coh[i-1][1]:
+            count += 1
+            if count == len(empty_list_coh):
+                empty_list_coh2 = "CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
+    if empty_list_coh2 == "CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY":
+        return "CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
+    else:
+        converted_coh = float(lis[1]) * float(forex)
+        return round(float(lis[0]), 1), round(converted_coh, 1)
+
+print(cashdata(1))
