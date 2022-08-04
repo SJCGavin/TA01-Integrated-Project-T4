22e57273cc7fb4fc135ebb8544f78cd60c979dc4
jingjingggggggg <S10221116@connect.np.edu.sg>
2022-08-04 16:36:27 +0800

    add comments

--- profit_loss.py
+++ profit_loss.py
@@ -21,12 +21,12 @@ def profit_loss_data(forex):
             for line in reader:
                 stuff = (line[0],line[3])
                 empty_list_pnl.append(list(stuff))
-
+    #
         count = 1
         for k in range(1,len(empty_list_pnl)):
             if float(empty_list_pnl[k][1]) >= float(empty_list_pnl[k-1][1]):
                 count += 1
-
+    #
         if count == len(empty_list_pnl):
             return "NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
         else:
@@ -35,12 +35,13 @@ def profit_loss_data(forex):
                     empty_list_pnl2.append(empty_list_pnl[i])
                 else:
                     pass
-
+    #
             for days_and_money in empty_list_pnl2:
                 days_and_money[0] = float(days_and_money[0])
                 days_and_money[1] = float(days_and_money[1]) * float(forex)
 
             return empty_list_pnl2
-
+    #
     except Exception as e:
         return f"AN ERROR HAS OCCURED. \nREASON: {e}"
+yay
