854af7f2f546e12cf27308cde7fc6373e034bf46
s10243563@connect.np.edu.sg <s10243563@connect.np.edu.sg>
2022-08-04 17:28:58 +0800

    asd

--- overheads.py
+++ overheads.py
@@ -1,37 +1,5 @@
-from asyncio import wait_for
-from bdb import effective
-from collections import deque
-from colorsys import rgb_to_yiq, yiq_to_rgb
-from ctypes import wstring_at
-from curses import erasechar
-from decimal import ROUND_HALF_EVEN
-from distutils.util import rfc822_escape
-from errno import EEXIST, EPWROFF
-from fcntl import F_GETFD
-from lib2to3.pgen2 import driver
-from lib2to3.pgen2.pgen import DFAState
-from locale import ERA_D_FMT
-from logging.handlers import RotatingFileHandler, TimedRotatingFileHandler
-from multiprocessing import Event
-from multiprocessing.sharedctypes import RawValue
-from os import removexattr
 from pathlib import Path
 import csv
-import re
-from readline import read_history_file
-from socket import EAI_SERVICE, TIPC_CFG_SRV
-from socketserver import DatagramRequestHandler
-from sys import _enablelegacywindowsfsencoding
-from tabnanny import verbose
-from tkinter import E, W
-from typing import ForwardRef
-from urllib.request import FTPHandler
-from wave import WAVE_FORMAT_PCM
-import weakref
-from winreg import REG_NOTIFY_CHANGE_ATTRIBUTES
-from wsgiref.util import FileWrapper
-from xml.dom.expatbuilder import theDOMImplementation
-from xml.sax.handler import feature_external_ges
 
 empty_list_overhead = []
 fp_overhead = Path.cwd()/"csv_reports"/"Overheads-day-45.csv"
@@ -56,4 +24,3 @@ def overhead(forex):
     
     except Exception as e:
         return f"AN ERROR HAS OCCURED. \nREASON: {e}"
-
